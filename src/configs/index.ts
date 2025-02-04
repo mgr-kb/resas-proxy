c73d38f9eb04fa6bc0aeed050ffdc44f3befa235
mgr-kb <koumgrars13@gmail.com>
2025-02-04 20:12:18 +0900

    configを追加

---
+++ src/configs/index.ts
@@ -0,0 +1,11 @@
+/**
+ * [NOTE]
+ * baseUrl, apiごとのendpointは環境によって変更される可能性もあるが、
+ * 今回は固定値として設定します。
+ */
+export const configs = {
+	baseUrl: "https://yumemi-frontend-engineer-codecheck-api.vercel.app",
+	prefectures: {
+		path: "/api/v1/prefectures",
+	},
+} as const;
