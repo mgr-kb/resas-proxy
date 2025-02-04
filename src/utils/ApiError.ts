60a1958784ad58a37448d070f240c9346e3fe192
mgr-kb <koumgrars13@gmail.com>
2025-02-04 20:37:30 +0900

    カスタムエラークラスにnameを追加

--- src/utils/ApiError.ts
+++ src/utils/ApiError.ts
@@ -1,13 +1,16 @@
 export class ApiError extends Error {
+	name: string;
 	status: number;
 	statusText: string;
 
 	constructor({
 		message,
+		name,
 		status,
 		statusText,
-	}: { message: string; status?: number; statusText?: string }) {
+	}: { message: string; name?: string; status?: number; statusText?: string }) {
 		super(message);
+		this.name = name || "ApiError";
 		this.status = status || 500;
 		this.statusText = statusText || "Internal Server Error";
 	}
